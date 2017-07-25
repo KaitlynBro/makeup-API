c514f30f541fe1c7d4b12156ae83473028cd9124
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-07-24 20:25:20 -0400

    error

---
+++ dev/scripts/app.js
@@ -0,0 +1,37 @@
+const makeupApp = {}
+
+makeupApp.init = function(){
+		makeupApp.getMakeup();
+};
+
+makeupApp.getMakeup = function(){
+		$.ajax({
+	  url: 'http://makeup-api.herokuapp.com/api/v1/products.json',
+	  method: 'GET',
+	  dataType: 'json',
+	}).then(function(res) {
+	  console.log(res);
+	  makeupApp.displayMakeup(res);
+	});
+};
+
+//makeupApp.displayMakeup = function(data){
+makeupApp.displayMakeup = function(data){
+		data.forEach(function(res){
+    	const brand = $('<h2>').text(res.brand);
+    	const name = $('<p>').text(res.name);
+    	const description = $('<p>').text(res.description);
+    	const price = $('<p>').text(res.price);
+    	const image = $('<img>').attr('src', res.image_link);
+
+
+		var makeupPiece = $('<div>').addClass('piece').append(brand, name, description, price, image);
+		$('.pieces').append(makeupPiece);
+	});
+};
+
+
+
+$(function(){
+		makeupApp.init();
+});
